c6e47560172e0563736df691dd4753f6e05f98bc
ilia.kashtanov <ilia.kashtanov@t-systems.com>
2026-02-17 23:19:39 +0300

    env

--- fromshelter_django/settings.py
+++ fromshelter_django/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'fromshelter_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),       # Your database name
+        'USER': os.getenv("DB_USER"),         # Your MySQL username
+        'PASSWORD': os.getenv("DB_PASSWORD"),     # Your MySQL password
+        'HOST': 'localhost',        # Or the host address if remote
+        'PORT': '3306',             # The MySQL port
     }
 }
 
